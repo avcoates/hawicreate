2eb7f5663700edef4ef49418fc5d0e1acba1654b
Alexander Coates <coatea62@uwosh.edu>
2019-08-22 23:27:23 -0500

    feat: initial material set up

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+  ]
+})
+export class MaterialModule {}
