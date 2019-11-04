727e2ab78fd91cd5c534c11eff1430f67b9cd4b6
avcoates <coatea62@uwosh.edu>
2019-11-03 22:29:02 -0600

    refactor(home): dumb down until slide show component is made

--- src/app/features/portfolio/components/home/home.component.ts
+++ src/app/features/portfolio/components/home/home.component.ts
@@ -1,92 +1,39 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { ImagesState } from 'src/app/state/images.state';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { GetFeaturedPhotos } from 'src/app/actions/images.actions';
-import { timer, combineLatest } from 'rxjs';
-import { map, tap, debounceTime } from 'rxjs/operators';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'hawicreate-app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger('changeImage', [
-      // ...
-      state('visible', style({
-        opacity: 1.0
-      })),
-      state('hidden', style({
-        opacity: 0.1
-      })),
-      transition('visible => hidden', [
-        animate('0.5s')
-      ]),
-      transition('hidden => visible', [
-        animate('0.5s')
-      ]),
-    ])
-  ],
+    selector: 'hawicreate-app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
-  public get isHidden$(): Observable<boolean> {
-    return this._isHidden.asObservable();
-  }
-  // tslint:disable-next-line: variable-name
-  public _isHidden = new BehaviorSubject<boolean>(false);
+    @Select(ImagesState.featuredImages)
+    public featuredImages$!: Observable<Array<string>>;
 
-  @Select(ImagesState.featuredImages)
-  public featuredImages$!: Observable<Array<string>>;
+    /*
+        TODO: For the sake of simplicity and getting a working feature,
+        only the last image in featured images will be used until I implement
+        a sildeshow component to fade new images in
+    */
+    public get imageIndex$(): Observable<number> {
+        return this.featuredImages$.pipe(
+                                        map(images => images.length ? images.length - 1 : null)
+                                    );
+    }
+    constructor(private store: Store) {
+        // this.store.dispatch(new GetFeaturedPhotos());
+    }
 
-  public state$: Observable<{ featuredImages: string[], imageIndex: number}>;
-  private imageSwitchTimer$ = timer(100, 5000);
+    public ngOnInit(): void {
 
-  // tslint:disable-next-line: variable-name
-  private _imageIndex = new BehaviorSubject<number>(0);
-  private get imageIndex$(): Observable<number> {
-    return this._imageIndex.asObservable();
-  }
-  constructor(private store: Store) {
-    const waitTime = 1000;
-    this.store.dispatch(new GetFeaturedPhotos());
-    this.state$ = combineLatest([this.featuredImages$, this.imageIndex$])
-      .pipe(
-        tap((this.fadeImageOutThenIn.bind(this))),
-        debounceTime(waitTime),
-        map(([featuredImages, imageIndex]) => ({ featuredImages, imageIndex })),
-        );
-  }
+    }
 
-  public ngOnInit(): void {
-    combineLatest([this.featuredImages$, this.imageSwitchTimer$])
-      .pipe(map(([featuredImages, imageIndex]) => ({ numberOfImages: featuredImages.length, imageIndex })))
-      .subscribe(({numberOfImages, imageIndex}) => {
-        const currentImageIndex = this._imageIndex.getValue();
-        if (currentImageIndex === numberOfImages - 1) {
-          this._imageIndex.next(0);
-        } else {
-          this._imageIndex.next(currentImageIndex + 1);
-        }
-      });
-  }
-
-  public ngOnDestroy(): void {
-  }
-
-  public onChangeImage(): void {
-    // this.isHidden = !this.isHidden;
-  }
-
-  private fadeImageOutThenIn(): void {
-    this._isHidden.next(false);
-    console.log(this._isHidden.getValue());
-    const timerRef = timer(1000, 1000).subscribe(() => { 
-      this._isHidden.next(true);
-      console.log(this._isHidden.getValue());
-      timerRef.unsubscribe();
-    });
-  }
+    public ngOnDestroy(): void {
+    }
 
 }
