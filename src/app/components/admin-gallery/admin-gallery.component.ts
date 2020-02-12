d6b1751db78c5531d4aa28b5ed5522054c8d1519
avcoates <coatea62@uwosh.edu>
2020-02-11 22:14:54 -0600

    feat: favicon with airstream font

--- src/app/components/admin-gallery/admin-gallery.component.ts
+++ src/app/components/admin-gallery/admin-gallery.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Select, Store } from '@ngxs/store';
+import { Select, Store, Actions } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { ArtPiece, Collection, ArtPieceDto } from '@admin/shared/models';
 import { FormBuilder, FormGroup, Validators, AbstractControl, FormArray } from '@angular/forms';
@@ -16,6 +16,7 @@ import { untilDestroyed } from 'ngx-take-until-destroy';
 import { isNullOrUndefined } from 'util';
 import { ArtPieceState } from '@admin/state/art-piece.state';
 import { ArtPieceApiService } from '@admin/services';
+import { SnackBarService } from '@admin/shared/services';
 
 @Component({
     selector: 'hc-admin-gallery',
@@ -31,7 +32,9 @@ export class AdminGalleryComponent implements OnInit, OnDestroy {
                 private store: Store,
                 private router: Router,
                 private artPieceApiService: ArtPieceApiService,
-                private matDialog: MatDialog
+                private matDialog: MatDialog,
+                private actions: Actions,
+                private snackBarService: SnackBarService
                 ) { }
 
 
@@ -41,6 +44,20 @@ export class AdminGalleryComponent implements OnInit, OnDestroy {
         console.log('hey');
         // this.collectionApiService.getAllCollcetions().pipe(tap(console.log)).subscribe();
         // this.onAddImage();
+
+        this.actions
+        .pipe(
+            filter(action => action.action instanceof AddArtPiece),
+            untilDestroyed(this)
+        )
+        .subscribe(action => {
+            console.log(action.status);
+            if (action.status === 'SUCCESSFUL') {
+                this.snackBarService.openSnackBar('Art piece added successfully!');
+            } else if (action.status === 'ERRORED' || action.status === 'CANCELED') {
+                this.snackBarService.openSnackBar('There was a problem added the art piece');
+            }
+        });
     }
 
     public ngOnDestroy(): void {
