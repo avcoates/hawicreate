9762096bb32c12787fa88ccd2226a45ca437222d
avcoates <coatea62@uwosh.edu>
2020-04-03 18:09:03 -0500

    feat: ngx a9

--- projects/hawicreate/src/app/app.module.ts
+++ projects/hawicreate/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { RECAPTCHA_V3_SITE_KEY,
 import { HttpClientModule } from '@angular/common/http';
 import { ContactService, RecaptchaService } from '../services';
 import { ArtPieceComponent } from './components/art-piece/art-piece.component';
-import { NgxGalleryModule } from 'ngx-gallery';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [
